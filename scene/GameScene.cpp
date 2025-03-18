ddaba66201a7d5e958abcd536d792b6dcdfd70b7
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-03-18 15:32:11 +0900

    Include 修正

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -3,14 +3,6 @@
 /// ECS
 #include "ECS/ECSManager.h"
 // component
-#include "component/BulletSpawner.h"
-#include "component/CharacterStatus.h"
-// system
-#include "system/CharacterOnCollision.h"
-#include "system/DeleteCharacterEntitySystem.h"
-#include "system/PlayerInputSystem.h"
-#include "system/ShotBulletSystem.h"
-
 GameScene::GameScene()
     : IScene("GameScene") {}
 
@@ -20,18 +12,12 @@ void GameScene::registerComponents() {
     IScene::registerComponents();
 
     ECSManager* ecsManager = ECSManager::getInstance();
-    ecsManager->registerComponent<BulletSpawner>();
-    ecsManager->registerComponent<CharacterStatus>();
 }
 
 void GameScene::registerSystems() {
     IScene::registerSystems();
 
     ECSManager* ecsManager = ECSManager::getInstance();
-    ecsManager->registerSystem<CharacterOnCollision>();
-    ecsManager->registerSystem<ShotBulletSystem>();
-    ecsManager->registerSystem<PlayerInputSystem>();
-    ecsManager->registerSystem<DeleteCharacterEntitySystem>();
 
     ecsManager->SortPriorityOrderSystems();
 }
