696f9518f1d6d1169910b8c5dfabd7b378923ec0
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-05-03 16:55:41 +0900

    json適応

--- code/component/Piller/FloatingFloorSpawner.cpp
+++ code/component/Piller/FloatingFloorSpawner.cpp
@@ -38,12 +38,12 @@ void FloatingFloorSpawner::Finalize() {}
 void to_json(nlohmann::json& _json, const FloatingFloorSpawner& _block) {
     _json["pillerSpace"]            = _block.positionHeight_;
     _json["columNumMax"]            = _block.columNumMax_;
+    _json["rowNum"]                 = _block.rowNumber_;
     _json["pillarHP"]               = _block.HPMax_;
     _json["FallCollisionMin"]       = _block.fallCollisionSizeMin_;
     _json["FallCollisionMax"]       = _block.fallCollisionSizeMax_;
     _json["fallCollisionCenterPos"] = _block.fallCollisionCenterPos_;
     _json["sideSpace"]              = _block.sideSpace_;
-    _json["rowNumber"]              = _block.rowNumber_;
     _json["revivalTime"]            = _block.revivalTime_;
     _json["ratio"]                  = _block.ratio_;
 }
@@ -51,12 +51,12 @@ void to_json(nlohmann::json& _json, const FloatingFloorSpawner& _block) {
 void from_json(const nlohmann::json& _json, FloatingFloorSpawner& _block) {
     _json.at("pillerSpace").get_to(_block.positionHeight_);
     _json.at("columNumMax").get_to(_block.columNumMax_);
+    _json.at("rowNum").get_to(_block.rowNumber_);
     _json.at("pillarHP").get_to(_block.HPMax_);
     _json.at("FallCollisionMin").get_to(_block.fallCollisionSizeMin_);
     _json.at("FallCollisionMax").get_to(_block.fallCollisionSizeMax_);
     _json.at("fallCollisionCenterPos").get_to(_block.fallCollisionCenterPos_);
     _json.at("sideSpace").get_to(_block.sideSpace_);
-    _json.at("rowNumber").get_to(_block.rowNumber_);
     _json.at("revivalTime").get_to(_block.revivalTime_);
     _json.at("ratio").get_to(_block.ratio_);
 }
