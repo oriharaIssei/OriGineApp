79848eeb1fb0418403717572fc582e2a38a5efb7
OriharaIssei <k023g0048@g.neec.ac.jp>
2026-03-14 18:09:47 +0900

    TimerStop時 GrayScaleをかけるように

--- code/system/effect/TimeScaleEffectSystem.cpp
+++ code/system/effect/TimeScaleEffectSystem.cpp
@@ -8,6 +8,8 @@
 // component
 #include "component/effect/post/GrayscaleComponent.h"
 #include "component/gimmick/TimeScaleEffectComponent.h"
+// system
+#include "system/postRender/GrayScaleEffect.h"
 
 using namespace OriGine;
 
@@ -21,6 +23,14 @@ void TimeScaleEffectSystem::Initialize() {
     deltaTimer->SetTimeScale("Effect", 1.f);
     deltaTimer->SetTimeScale("Camera", 1.f);
     deltaTimer->SetTimeScale("Timer", 1.f);
+
+    // GrayScale Effect Entityの作成
+    grayScaleEffectEntityHandle_  = CreateEntity("GrayscaleEffect", false);
+    Entity* grayScaleEffectEntity = GetEntity(grayScaleEffectEntityHandle_);
+    grayScaleEffectEntity->SetShouldSave(false); // セーブ対象外
+    AddComponent<GrayscaleComponent>(grayScaleEffectEntityHandle_);
+
+    GetScene()->GetSystem(nameof<GrayscaleEffect>())->AddEntity(grayScaleEffectEntityHandle_);
 }
 void TimeScaleEffectSystem::Finalize() {}
 
@@ -49,6 +59,51 @@ void TimeScaleEffectSystem::Update() {
         }
     }
 
+    // タイムスケールタイマーの更新
+    UpdateScaleTimer();
+
+    // fade In/Out の更新
+    float leftTime = 0.f;
+    for (const auto& [key, timeScaleComponent] : stopTimesByTimeScale_) {
+        if (timeScaleComponent.scaleTimer > 0.f) {
+            leftTime = (std::max)(leftTime, timeScaleComponent.scaleTimer);
+        }
+    }
+
+    // 有効なタイムスケールが存在する場合はグレースケールエフェクトを有効化、存在しない場合は無効化
+    DeltaTimer* deltaTimer = Engine::GetInstance()->GetDeltaTimer();
+    if (leftTime > 0.f) {
+        effectElapsedTime_ += deltaTimer->GetDeltaTime();
+    } else {
+        effectElapsedTime_ = 0.f;
+    }
+
+    float fadeInRatio = 1.f;
+    if (fadeInTime_ > 0.f) {
+        fadeInRatio = std::clamp(effectElapsedTime_ / fadeInTime_, 0.f, 1.f);
+    }
+
+    float fadeOutRatio = 0.f;
+    if (leftTime > 0.f) {
+        if (fadeOutTime_ > 0.f) {
+            fadeOutRatio = std::clamp(leftTime / fadeOutTime_, 0.f, 1.f);
+        } else {
+            fadeOutRatio = 1.f;
+        }
+    }
+
+    float effectAmount = (std::min)(fadeInRatio, fadeOutRatio);
+
+    GrayscaleComponent* grayScaleEffect = GetComponent<GrayscaleComponent>(grayScaleEffectEntityHandle_);
+    if (grayScaleEffect) {
+        grayScaleEffect->SetIsEnabled(leftTime > 0.f);
+        grayScaleEffect->SetAmount(effectAmount);
+    } else {
+        LOG_ERROR("GrayscaleComponent is not found.");
+    }
+}
+
+void TimeScaleEffectSystem::UpdateScaleTimer() {
     // タイムスケールの適用
     DeltaTimer* deltaTimer = Engine::GetInstance()->GetDeltaTimer();
     for (const auto& [key, timeScaleComponent] : stopTimesByTimeScale_) {
